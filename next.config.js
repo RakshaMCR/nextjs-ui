c40959ef9564e17de9251d0d8406e7aba0283615
RakshaMCR <154351265+RakshaMCR@users.noreply.github.com>
2024-01-18 14:55:19 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+    reactStrictMode: true,
+    output: {
+      dir: 'out', // Output directory
+      html: true, // Generate HTML files
+      static: true, // Generate static files (CSS, JS, images)
+    },
+   };
