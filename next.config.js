ca8c895a4ce9d9806a7e509f3354e12e81613b17
RakshaMCR <154351265+RakshaMCR@users.noreply.github.com>
2024-01-19 01:41:03 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-/** 
-* @type {import('next').NextConfig}
-*/
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
- reactStrictMode: true,
- output: "standalone",
-};
+ output: 'export',
+}
+
 module.exports = nextConfig
